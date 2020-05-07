83535040dcb0d69e84a19ae4064726c63b55e28a
NguyenThanhAI <37617026+NguyenThanhAI@users.noreply.github.com>
2020-05-07 17:49:49 +0700

    Add Tentative Tracks

--- vehicle_tracking/vehicle_track.py
+++ vehicle_tracking/vehicle_track.py
@@ -3,9 +3,26 @@ from collections import deque
 import numpy as np
 
 
+class TentativeTrack(object):
+
+    def __init__(self, score, bbox, positions, class_id, tentative_id, tentative_steps_before_accepted, max_traject_steps, parking_ground, cam):
+        self.score = score
+        self.bbox = bbox
+        self.positions = positions
+        self.class_id = class_id
+        self.tentative_id = tentative_id
+        self.tentative_steps = 0
+        self.tentative_steps_before_accepted = tentative_steps_before_accepted
+        self.traject_pos = deque([bbox.copy()], maxlen=max_traject_steps)
+        self.traject_vel = deque()
+        self.time_stamp = deque([time.time()], maxlen=max_traject_steps)
+        self.parking_ground = parking_ground
+        self.cam = cam
+
+
 class VehicleTrack(object):
 
-    def __init__(self, score, bbox, positions, class_id, track_id, inactive_steps_before_removed, max_traject_steps, parking_ground, cam):
+    def __init__(self, score, bbox, positions, class_id, track_id, inactive_steps_before_removed, traject_pos, traject_vel, time_stamp, parking_ground, cam):
         self.score = score
         self.bbox = bbox  # x_min, y_min, x_max, y_max
         self.positions = positions # Tập hợp các điểm [y1, y2, ..., yn], [x1, x2, ..., xn] nằm trong vehicle mask
@@ -13,9 +30,9 @@ class VehicleTrack(object):
         self.track_id = track_id
         self.inactive_steps = 0
         self.inactive_steps_before_removed = inactive_steps_before_removed
-        self.traject_pos = deque([bbox.copy()], maxlen=max_traject_steps)
-        self.traject_vel = deque()
-        self.time_stamp = deque([time.time()], maxlen=max_traject_steps)
+        self.traject_pos = traject_pos.copy()
+        self.traject_vel = traject_vel.copy()
+        self.time_stamp = time_stamp.copy()
         self.birth_time = [time.time()]
         self.alive_time = []
         self.parking_ground = parking_ground
--- vehicle_tracking/vehicle_tracker.py
+++ vehicle_tracking/vehicle_tracker.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 import cv2
-from vehicle_tracking.vehicle_track import VehicleTrack
+from vehicle_tracking.vehicle_track import TentativeTrack, VehicleTrack
 from vehicle_tracking.utils import iou_mat
 from code_timing_profiling.profiling import profile
 from code_timing_profiling.timing import timethis
@@ -17,6 +17,7 @@ class VehicleTracker:
                  reid_iou_threshold,
                  max_traject_steps,
                  parking_ground,
+                 tentative_steps_before_accepted=30,
                  cam="cam_1",
                  shape=(720, 1280)):
 
@@ -30,13 +31,16 @@ class VehicleTracker:
         #self.do_align = do_align
         self.max_traject_steps = max_traject_steps
         self.parking_ground = parking_ground
+        self.tentative_steps_before_accepted = tentative_steps_before_accepted
         self.cam = cam
         self.shape = shape
         self.positions_mask = -1 * np.ones(shape=self.shape, dtype=np.int16)
         self.square_of_mask = OrderedDict() # Number of pixel (square) of each track id mask
 
+        self.tentative_tracks = []
         self.active_tracks = []
         self.inactive_tracks = []
+        self.tentative_track_num = 0
         self.track_num = 0
         #self.im_index = 0
         #self.results = {}
@@ -46,6 +50,7 @@ class VehicleTracker:
         self.inactive_tracks = []
 
         if hard:
+            self.tentative_track_num = 0
             self.track_num = 0
             #self.results = {}
             #self.im_index = 0
@@ -56,20 +61,44 @@ class VehicleTracker:
             t.bbox = t.traject_pos[-1]
         self.inactive_tracks += tracks
 
-    def add_new_tracks(self, vehicle_detections_list):
-        """Initiailizes new Track objectss and save them"""
-        num_new = len(vehicle_detections_list)
+    def tentative_tracks_to_active(self, tracks):
+        """Initializes new Track objects and save them"""
+        num_new = len(tracks)
         for i in range(num_new):
-            self.active_tracks.append(VehicleTrack(score=vehicle_detections_list[i].score,
-                                                   bbox=vehicle_detections_list[i].bbox,
-                                                   positions=vehicle_detections_list[i].positions,
-                                                   class_id=vehicle_detections_list[i].class_id,
+            self.active_tracks.append(VehicleTrack(score=tracks[i].score,
+                                                   bbox=tracks[i].bbox,
+                                                   positions=tracks[i].positions,
+                                                   class_id=tracks[i].class_id,
                                                    track_id=self.track_num + i,
                                                    inactive_steps_before_removed=self.inactive_steps_before_removed,
-                                                   max_traject_steps=self.max_traject_steps,
+                                                   traject_pos=tracks[i].traject_pos,
+                                                   traject_vel=tracks[i].traject_vel,
+                                                   time_stamp=tracks[i].time_stamp,
                                                    parking_ground=self.parking_ground,
                                                    cam=self.cam))
         self.track_num += num_new
+
+    def add_new_tentative_tracks(self, vehicle_detections_list):
+        """Initiailizes new Tentative Track objectss and save them"""
+        num_new = len(vehicle_detections_list)
+        for i in range(num_new):
+            self.tentative_tracks.append(TentativeTrack(score=vehicle_detections_list[i].score,
+                                                        bbox=vehicle_detections_list[i].bbox,
+                                                        positions=vehicle_detections_list[i].positions,
+                                                        class_id=vehicle_detections_list[i].class_id,
+                                                        tentative_id=self.tentative_track_num + i,
+                                                        tentative_steps_before_accepted=self.tentative_steps_before_accepted,
+                                                        max_traject_steps=self.max_traject_steps,
+                                                        parking_ground=self.parking_ground,
+                                                        cam=self.cam))
+        self.tentative_track_num += num_new
+
+    def get_tentative_boxes(self):
+        if len(self.tentative_tracks) >= 1:
+            bboxes = [t.bbox for t in self.tentative_tracks]
+        else:
+            bboxes = []
+        return bboxes
 
     def get_active_boxes(self):
         if len(self.active_tracks) >= 1:
@@ -99,6 +128,37 @@ class VehicleTracker:
             bboxes = []
         return bboxes
 
+    def match_reid_iou_tentative(self, vehicle_detections):
+        tentative_tracks_bboxes = self.get_tentative_boxes()
+        detections_bboxes = [detection.bbox for detection in vehicle_detections]
+
+        iou_matrix = iou_mat(tentative_tracks_bboxes, detections_bboxes)
+        iou_matrix = np.where(iou_matrix > self.reid_iou_threshold, iou_matrix, 0)
+
+        row_ind, col_ind = linear_sum_assignment(iou_matrix, maximize=True)
+
+        matched_detection_indx = []
+        matched_track_indx = []
+
+        for r, c in zip(row_ind, col_ind):
+            if iou_matrix[r, c] >= self.reid_iou_threshold and self.tentative_tracks[r].class_id == vehicle_detections[c].class_id:
+                matched_track_indx.append(r)
+                matched_detection_indx.append(c)
+                t = self.tentative_tracks[r]
+                t.bbox = vehicle_detections[c].bbox
+                t.score = vehicle_detections[c].score
+                t.positions = vehicle_detections[c].positions
+                t.time_stamp.append(time.time())
+        unmatched_tracks = [self.tentative_tracks[i] for i in range(len(self.tentative_tracks)) if i not in matched_track_indx]
+        unmatched_detections = [vehicle_detections[j] for j in range(len(vehicle_detections)) if j not in matched_detection_indx]
+
+        #self.tentative_tracks = [self.tentative_tracks[k] for k in matched_track_indx]
+        for t in unmatched_tracks:
+            self.tentative_tracks.remove(t)
+
+        return unmatched_detections
+
+
     def match_reid_iou_active(self, vehicle_detections):
         active_tracks_bboxes = self.get_active_boxes()
         detections_bboxes = [detection.bbox for detection in vehicle_detections]
@@ -121,7 +181,7 @@ class VehicleTracker:
                 t.positions = vehicle_detections[c].positions
                 t.time_stamp.append(time.time())
         unmatched_tracks = [self.active_tracks[i] for i in range(len(self.active_tracks)) if i not in matched_track_indx]
-        unmatched_detections = [vehicle_detections[j] for j in range(len(vehicle_detections)) if j not in matched_track_indx]
+        unmatched_detections = [vehicle_detections[j] for j in range(len(vehicle_detections)) if j not in matched_detection_indx] # Huhu, đáng ra phải là matched_detection_indx nhưng viết nhầm thành matched_track_indx, giờ mới phát hiện ra
 
         self.active_tracks = [self.active_tracks[k] for k in matched_track_indx]
 
@@ -180,6 +240,19 @@ class VehicleTracker:
 
             self.motion_step(t)
 
+        for t in self.tentative_tracks:
+            if len(t.traject_pos) == 1:
+                continue
+            last_bboxes = t.traject_pos
+            moments = t.time_stamp
+
+            vs = np.asarray([(p2 - p1) / (t2 - t1) for p1, p2, t1, t2 in zip(list(last_bboxes), list(last_bboxes)[1:], list(moments), list(moments)[1:])], dtype=np.float)
+            vs = np.mean(vs)
+
+            t.traject_vel.append(vs)
+
+            self.motion_step(t)
+
     @timethis
     def step(self, vehicle_detections):
         for t in self.active_tracks:
@@ -188,7 +261,21 @@ class VehicleTracker:
             else: # If len of t.traject_pos is 1 means that track just been created and t.bbox was already appended to t.traject_ps
                 pass
 
+        for t in self.tentative_tracks:
+            if len(t.traject_pos) > 1:
+                t.traject_pos.append(t.bbox.copy())
+            else:
+                pass
+
         self.motion()
+
+        if len(self.tentative_tracks) > 0 and len(vehicle_detections) > 0:
+            print("14, {}, {}".format(len(self.tentative_tracks), len(vehicle_detections)), end=", ")
+            vehicle_detections = self.match_reid_iou_tentative(vehicle_detections=vehicle_detections)
+            print("{}, {}".format(len(self.tentative_tracks), len(vehicle_detections)))
+        elif len(self.tentative_tracks) > 0 and len(vehicle_detections) == 0:
+            print("15, {}, {}".format(len(self.tentative_tracks), len(vehicle_detections)))
+            self.tentative_tracks.clear()
 
         if len(self.active_tracks) > 0 and len(vehicle_detections) > 0:
             unmatched_active_tracks, unmatched_detections = self.match_reid_iou_active(vehicle_detections=vehicle_detections)
@@ -213,7 +300,7 @@ class VehicleTracker:
 
             if len(unmatched_detections) > 0:
                 #print("6")
-                self.add_new_tracks(vehicle_detections_list=unmatched_detections)
+                self.add_new_tentative_tracks(vehicle_detections_list=unmatched_detections)
             else:
                 #print("7")
                 pass
@@ -228,16 +315,25 @@ class VehicleTracker:
                 unmatched_detections = self.match_reid_iou_inactive(vehicle_detections=vehicle_detections)
                 if len(unmatched_detections) > 0:
                     #print("10")
-                    self.add_new_tracks(vehicle_detections_list=unmatched_detections)
+                    self.add_new_tentative_tracks(vehicle_detections_list=unmatched_detections)
                 else:
                     #print("11")
                     pass
             else:
                 #print("12")
-                self.add_new_tracks(vehicle_detections_list=vehicle_detections)
+                self.add_new_tentative_tracks(vehicle_detections_list=vehicle_detections)
         else:
             #print("13")
             pass
+
+        tentative_to_active = []
+        for t in self.tentative_tracks:
+            t.tentative_steps += 1
+            if t.tentative_steps > t.tentative_steps_before_accepted:
+                tentative_to_active.append(t)
+        self.tentative_tracks_to_active(tentative_to_active)
+        for track in tentative_to_active:
+            self.tentative_tracks.remove(track)
 
         remove_inactive = []
         for t in self.inactive_tracks:
